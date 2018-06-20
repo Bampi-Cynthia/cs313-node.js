45cedc48f19beac0937f2f707416ee600ad310d7
Bampi-Cynthia <bam15003@byui.edu>
2018-06-20 16:38:52 -0600

    prove 09 assignment

--- index.js
+++ index.js
@@ -1,12 +1,160 @@
-const http = require('http');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
-let server = http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('My name is Cyndi!');
-  res.end();
-});
 
-// process.env.PORT is important! Heroku won't work without it!
-server.listen(process.env.PORT || 3000, function() {
-  console.log('Now listening!');
-});
+app.use('/', express.static('static'));
+
+app.use(bodyParser());
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+function calcPostageLettersStamped(weight){
+	if (weight == 1){
+		return 0.50;
+	}
+	else if(weight == 2){
+		return 0.71;
+	}
+	else if(weight == 3){
+		return 0.92;
+	}
+	else if(weight == 3.5){
+		return 1.13;
+	}
+	else{
+		//return Not Valid;
+	}
+}
+
+function calcPostageLettersMetered(weight){
+	if (weight == 1){
+		return 0.47;
+	}
+	else if(weight == 2){
+		return 0.68;
+	}
+	else if(weight == 3){
+		return 0.89;
+	}
+	else if(weight == 3.5){
+		return 1.10;
+	}
+	else{
+		//return Not Valid;
+	}
+}
+
+function calcPostageLargeEnvelopes(weight){
+	if (weight == 1){
+		return 0.98;
+	}
+	else if(weight == 2){
+		return 1.19;
+	}
+	else if(weight == 3){
+		return 1.40;
+	}
+	else if(weight == 4){
+		return 1.61;
+	}
+	else if(weight == 5){
+		return 1.82;
+	}
+	else if(weight == 6){
+		return 2.03;
+	}
+	else if(weight == 7){
+		return 2.24;
+	}
+	else if(weight == 8){
+		return 2.45;
+	}
+	else if(weight == 9){
+		return 2.66;
+	}
+	else if(weight == 10){
+		return 2.87;
+	}
+	else if(weight == 11){
+		return 3.08;
+	}
+	else if(weight == 12){
+		return 3.29;
+	}
+	else if(weight == 13){
+		return 3.50;
+	}
+	else{
+		//return Not Valid;
+	}
+}
+
+function calPostageFirstClass(weight){
+	if (weight == 1){
+		return 3.50;
+	}
+	else if(weight == 2){
+		return 3.50;
+	}
+	else if(weight == 3){
+		return 3.50;
+	}
+	else if(weight == 4){
+		return 3.50;
+	}
+	else if(weight == 5){
+		return 3.75;
+	}
+	else if(weight == 6){
+		return 3.75;
+	}
+	else if(weight == 7){
+		return 3.75;
+	}
+	else if(weight == 8){
+		return 3.75;
+	}
+	else if(weight == 9){
+		return 4.10;
+	}
+	else if(weight == 10){
+		return 4.45;
+	}
+	else if(weight == 11){
+		return 4.80;
+	}
+	else if(weight == 12){
+		return 5.15;
+	}
+	else if(weight == 13){
+		return 5.50;
+	}
+	else{
+		//return Not Valid;
+	}
+}
+
+
+
+app.post('/getData', function(request, response){
+
+	//
+	var result,
+	weight = Number(request.body.weight);
+
+	switch(Number(request.body.type)){
+		case 1 :
+		result = calcPostageLettersStamped(weight);
+		break;
+		case 2 :
+		result = calcPostageLettersMetered(weight);
+		break;
+		case 3 :
+		result = calcPostageLargeEnvelopes(weight);
+		break;
+		case 4 :
+		result = calPostageFirstClass(weight);
+		break;
+	}
